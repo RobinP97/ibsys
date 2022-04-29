ad629789b23ba13f669fe34b82aaaec5083c798c
RobinP97 <73746547+RobinP97@users.noreply.github.com>
2022-04-29 17:52:58 +0200

    rename classes to model

---
+++ src/app/model/export/input.ts
@@ -0,0 +1,8 @@
+import { qualitycontrol } from "./qualitycontrol";
+import { sellwish } from "./sellwish";
+
+export interface input
+{
+    qualitycontrol: qualitycontrol;
+    sellwish: sellwish 
+}
---
+++ src/app/model/export/qualitycontrol.ts
@@ -0,0 +1,6 @@
+export interface qualitycontrol
+{
+    type: string;
+    losequanity: number;
+    delay: number;
+}

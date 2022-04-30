7707f982b4c9053fa0243ecd96dd33ac228809a5
Dennis Griebler <grde1017@hs-karlsruhe.de>
2022-05-01 01:41:04 +0200

    Prettier

--- src/app/pipes/prettyprint.pipe.ts
+++ src/app/pipes/prettyprint.pipe.ts
@@ -1,14 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'prettyprint'
+  name: 'prettyprint',
 })
 export class PrettyPrintPipe implements PipeTransform {
-
   transform(value: unknown, ...args: unknown[]): unknown {
     return JSON.stringify(value, null, 2)
-    .replace(/ /g, '&nbsp;') // note the usage of `/ /g` instead of `' '` in order to replace all occurences
-    .replace(/\n/g, '<br/>'); // same here
+      .replace(/ /g, '&nbsp;') // note the usage of `/ /g` instead of `' '` in order to replace all occurences
+      .replace(/\n/g, '<br/>'); // same here
   }
-
 }

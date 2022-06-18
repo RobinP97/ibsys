922ff25b10424f45bd7454765c33e8cd3aa53a63
Dennis Griebler <grde1017@hs-karlsruhe.de>
2022-06-18 20:56:40 +0200

    add FormsModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import {
   TranslateService,
 } from '@ngx-translate/core';
 
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -41,6 +42,7 @@ export function ApplicationInitializerFactory(
     CommonModule,
     HttpClientModule,
     MatToolbarModule,
+    FormsModule,
     MatIconModule,
     MatButtonModule,
     TranslateModule.forChild({
@@ -54,6 +56,7 @@ export function ApplicationInitializerFactory(
   ],
   exports: [
     CommonModule,
+    FormsModule,
     HttpClientModule,
     MatToolbarModule,
     MatIconModule,
